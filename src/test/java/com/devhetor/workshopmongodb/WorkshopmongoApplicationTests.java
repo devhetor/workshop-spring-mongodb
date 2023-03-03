e1183ccf7913145a755ab2217840fc6f079da6bb
heitor <heitorbrandolin@hotmail.com>
2023-03-03 15:28:41 -0300

    Project created

---
+++ src/test/java/com/devhetor/workshopmongodb/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhetor.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
