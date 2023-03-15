6b6c3e64cde7bb8039d7a175af98af4b531f7748
heitor <heitorbrandolin@hotmail.com>
2023-03-15 15:35:22 -0300

    Database instantiation operation

---
+++ src/main/java/com/devhetor/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,38 @@
+package com.devhetor.workshopmongodb.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.devhetor.workshopmongodb.domain.User;
+import com.devhetor.workshopmongodb.repository.UserRepository;
+
+
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+	}
+
+}
